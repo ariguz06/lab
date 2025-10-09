98b98cfaecefc81c5a309658b1abadc8c5631219
pat-mart <martinp@northshoreschools.org>
2025-10-08 22:16:09 -0400

    adjacency matrix parsing w/dict and minnesota data

--- main.py
+++ main.py
@@ -18,18 +18,48 @@ def bfs(start: Vertex):
                     queue.append(neighbor)
     
     return order
+
+'''
+parses .mtx file and returns adjacency matrix 
+'''
+def parse_mtx(file_path: str, start_line: int = 16):
+    
+    adjacency_matrix = {}
+    
+    with open(file_path, 'r') as file:
+        for line in file:
+            if not line.strip() or line.startswith('%'):
+                continue
+            
+            split = line.strip().split()
+            if len(split) == 3:
+                continue
+            
+            u, v = map(int, line.split())
+            
+            if u not in adjacency_matrix:
+                adjacency_matrix[u] = {}
+                
+            adjacency_matrix[u][v] = 1
+    
+    return adjacency_matrix
+
+
+def generate_graph(adjacency_matrix: dict):
+    seen = {}
+    
+    for u, v in adjacency_matrix.items():
+        if not u in seen:
+            print(u)
+            seen[u] = Vertex(key=str(u))
         
+        seen[u].neighbors.append(Vertex(key=str(v)))
+    
+    return seen
+            
 
 if __name__ == "__main__":
-    a = Vertex(key="a")
-    b = Vertex(key="b")
-    c = Vertex(key="c")
-    d = Vertex(key="d")
-    e = Vertex(key="e")
+    mn_mtx = parse_mtx("include/road-minnesota/road-minnesota.mtx")
+    graph = generate_graph(mn_mtx)
     
-    a.neighbors = [b, c, e]
-    e.neighbors = [a,b]
-    c.neighbors = [a]
-    b.neighbors = [c]
-    
-    print("BFS traversal: ", bfs(a))
+    bfs(graph["7"])
